7305f58972072ffd2b13c47d14e54c9f4373e34f
Igor Manassypov <imanassy@cisco.com>
2023-04-21 14:50:26 -0400

    revised message aggregation and file output method

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webex-room-messages
-Simple python script to collect all messages from a room to a file
+Simple python script to collect all Webex Space messages from a room to a file
+---
+## Usage
+`python webex-room-messages.py --room="<room_name>" --file="<file_name.json>" --api="<webex_api_bearer token>"`
--- webex-room-messages.py
+++ webex-room-messages.py
@@ -1,5 +1,17 @@
+# Igor Manassypov
+# Sr Systems Architect, Cisco Systems
+# Apr 2023
+#
+# Simple script to extract complete set of messages 
+# from a given webex message space by its name
+# the script organizes the messages by message created date field
+#
+# usage: python webex-room-messages.py --room="<room_name>" --file="<file_name.json>" --api="<webex_api_bearer token>"
+# get your webex api bearer token here: https://developer.webex.com/docs/getting-started under 'Your Personal Access Token'
+
 import requests
 import sys, argparse
+import json
 
 argv = sys.argv[1:]
 
@@ -32,19 +44,20 @@ def get_room_id (webex_rooms_json, room_name):
 def get_messages (room_id):
     if room_id is not None:
         webex_messages = webex_session.get(url=WEBEX_API+"messages?roomId={}".format(room_id))
-        webex_messages_list = (webex_messages.json())['items']
+        webex_messages_json = webex_messages.json()
         pages = 0
         while webex_messages.headers.get('link'):
             link = (webex_messages.headers['link']).split(';')[0][1:-1]
             webex_messages = webex_session.get(url=link)
-            webex_messages_list.append((webex_messages.json())['items'])
+            webex_messages_json['items'].extend((webex_messages.json())['items'])
             pages += 1
-            print('message count {}, collecting messages page: {} link {}'.format(len(webex_messages_list),pages, link))
-    return webex_messages_list
+            print('message count {}, collecting messages page: {} link {}'.format(len(webex_messages_json['items']),pages, link))
+    webex_messages_json['items'].sort(key=lambda x: x['created'])
+    return webex_messages_json
 
 def dump_to_file(webex_messages, filename):
     with open(filename, 'w') as f:
-        f.write(str(webex_messages))
+        json.dump(webex_messages, f)
 
 webex_rooms = webex_session.get(url=WEBEX_API+"rooms")
 if webex_rooms.status_code != 200:
@@ -65,4 +78,4 @@ webex_messages = get_messages(room_id)
 
 dump_to_file(webex_messages, FILE_OUT)
 
-print ('wrote {} messages to file {}'.format(len(webex_messages), FILE_OUT))
+print ('wrote {} messages to file {}'.format(len(webex_messages['items']), FILE_OUT))
